1d8fba5b85b711bc9c146cd8ad5481b3dde84612
Umbertolio <antonus777@gmail.com>
2021-03-22 21:06:35 +0200

    Add files via upload

---
+++ Fix/data.lua
@@ -0,0 +1,54 @@
+if settings.startup["tech-crafting"].value then
+	require("prototypes.tech-crafting")
+end
+if settings.startup["tech-reach"].value then
+	require("prototypes.tech-reach")
+end
+if settings.startup["tech-inventory"].value then
+	require("prototypes.tech-inventory")
+end
+if settings.startup["tech-health"].value then
+	require("prototypes.tech-health")
+end
+if settings.startup["magazine-speed"].value then
+	require("prototypes.magazine-speed")
+end
+if settings.startup["magazine-damage"].value then
+	require("prototypes.magazine-damage")
+end
+if settings.startup["laser-speed"].value then
+	require("prototypes.laser-speed")
+end
+if settings.startup["laser-damage"].value then
+	require("prototypes.laser-damage")
+end
+if settings.startup["rocket-damage"].value then
+	require("prototypes.rocket-damage")
+end
+if settings.startup["rocket-shooting-speed"].value then
+	require("prototypes.rocket-shooting-speed")
+end
+if settings.startup["artillery-range"].value then
+	require("prototypes.artillery-range")
+end
+if settings.startup["tech-shotgun-damage"].value then
+	require("prototypes.tech-shotgun-damage")
+end
+if settings.startup["tech-shotgun-speed"].value then
+	require("prototypes.tech-shotgun-speed")
+end
+if settings.startup["tech-movement"].value then
+	require("prototypes.tech-movement")
+end
+if settings.startup["tech-mining-drill"].value then
+	require("prototypes.tech-mining-drill")
+end
+if settings.startup["tech-mining"].value then
+	require("prototypes.tech-mining")
+end
+if settings.startup["lab-productivity"].value then
+	require("prototypes.lab-productivity")
+end
+if settings.startup["tech-research"].value then
+	require("prototypes.tech-research")
+end
---
+++ Fix/settings.lua
@@ -0,0 +1,110 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "tech-reach",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-inventory",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-health",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-crafting",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "rocket-shooting-speed",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "rocket-damage",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "magazine-speed",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "magazine-damage",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "laser-speed",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "laser-damage",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "artillery-range",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-shotgun-speed",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-shotgun-damage",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-movement",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-mining-drill",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-mining",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "lab-productivity",
+        setting_type = "startup",
+        default_value = true
+    },
+    {
+        type = "bool-setting",
+        name = "tech-research",
+        setting_type = "startup",
+        default_value = true
+    }
+})
